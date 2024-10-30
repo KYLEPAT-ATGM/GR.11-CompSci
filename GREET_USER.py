e54a49c19f9442d627d1e196cfcf68de336f5315
KYLEPAT-ATGM <kylepat234@gmail.com>
2024-10-30 13:13:58 -0400

    Delete GREET_USER.py

--- GREET_USER.py
+++
@@ -1,6 +0,0 @@
-def greet_user(name):
-    """Prints a greeting message to the user."""
-    """The 'name' parameter is a local variable that is said to be greeted."""
-    """No value is being returned in this function."""
-    print(f"Hello, {name}!")
-greet_user("Kyle")
